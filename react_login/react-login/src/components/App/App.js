72d39a90e87b2d66bc57c4379771db011b60692a
Furt_Tech <none@none.com>
2023-11-21 20:15:54 -0600

    fix mistake with outdated routes code

--- react_login/react-login/src/components/App/App.js
+++ react_login/react-login/src/components/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import './App.css';
 import Dashboard from '../Dashboard/Dashboard';
@@ -19,14 +19,10 @@ function App() {
     <div className="wrapper">
       <h1>Application</h1>
       <BrowserRouter>
-       
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/preferences">
-            <Preferences />
-          </Route>
-      
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/preferences" element={<Preferences />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
